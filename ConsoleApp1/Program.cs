819ee3d8ba5acebe572a735cd33ffcb6495e14a0
Mario Garcia Valdez <mariosky@gmail.com>
2020-11-25 12:57:59 -0800

    Excamación

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("¡Hola Mundo!");
             Console.ReadKey();
         }
     }
